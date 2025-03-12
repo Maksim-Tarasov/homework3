e1721758ea857b2b2e28f3543d44b0ee5a5e01a6
admin <m89606209505@yandex.ru>
2025-03-12 10:22:24 +0300

    три задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //задание 1
+        int myInt = 1876456342;
+        System.out.println("Значение переменной myInt с типом int равно" + myInt);
+        byte myByte = -14;
+        System.out.println("значение переменной myByte с типом byte равно" + myByte);
+        short myShort = -12127;
+        System.out.println("значение переменной myShort с типом short равно" + myShort);
+        long myLong = 378645656;
+        System.out.println("значение переменной myLong с типом long равно" + myLong);
+        float myFloat = 5.228f;
+        System.out.println("значение переменной myFloat с типом float равно" + myFloat);
+        double myDouble = 1.234543;
+        System.out.println("значение переменной myDouble с типом double равно" + myDouble);
+        //задание 2
+        float firstFlot = 27.12f;
+        long firstLong = 987678965549L;
+        double firstDouble = 2.786;
+        short firstShort = 569;
+        int firstInt = -159;
+        short firstShortTwo = 27897;
+        byte firstByte = 67;
+        //задание 3
+        int firstClass = 23;
+        int secondClass = 27;
+        int threeClass = 30;
+        int paper = 480;
+        System.out.println("на каждого ученика рассчитано" + paper / (firstClass + secondClass + threeClass));
+        //задание 4
+
+
+
     }
 }
